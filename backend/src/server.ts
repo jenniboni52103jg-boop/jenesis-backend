ab7297fdc88d6511671edaaec3691e09b3ba023e
jennifer.52103@hotmail.com <Jen21Fabrizio11>
2026-05-15 07:32:06 +0200

    fix cloudinary env names

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -80,17 +80,17 @@ fal.config({
 });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
 });
 //console.log("CLOUD NAME:", process.env.CLOUD_NAME);
 //console.log("API KEY:", process.env.CLOUD_API_KEY);
 //console.log("SECRET:", process.env.CLOUD_API_SECRET);
-console.log("CLOUD NAME EXISTS:", !!process.env.CLOUD_NAME);
-console.log("API KEY EXISTS:", !!process.env.CLOUD_API_KEY);
-console.log("SECRET EXISTS:", !!process.env.CLOUD_API_SECRET);
+console.log("CLOUD NAME EXISTS:", !!process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY EXISTS:", !!process.env.CLOUDINARY_API_KEY);
+console.log("SECRET EXISTS:", !!process.env.CLOUDINARY_API_SECRET);
 console.log("CLOUDINARY CONFIG:", cloudinary.config());
 
 const app = express();
